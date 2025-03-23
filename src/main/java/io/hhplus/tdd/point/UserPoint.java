d4617ac240d62799ff003f21c1014ddd654bde2d
seokho <seokho7799@gmail.com>
2025-03-23 22:48:32 +0900

    feat: 포인트 조회 기능 추가

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -6,7 +6,13 @@ public record UserPoint(
         long updateMillis
 ) {
 
+    private static final long MAX_POINT = 100_000;
+
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
+    }
+
+    public static long maxPoint() {
+        return MAX_POINT;
     }
 }
