86fecf1abe314898ec90e0fe053d793234ac22cf
seokho <seokho7799@gmail.com>
2025-03-25 22:17:43 +0900

    feat: 포인트, 포인트 내역 조회 기능 추가

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,23 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PointService {
+
+  private final UserPointTable userPointTable;
+  private final PointHistoryTable pointHistoryTable;
+
+  public UserPoint selectById(long userId) {
+    return userPointTable.selectById(userId);
+  }
+
+  public List<PointHistory> selectAllByUserId(long userId) {
+    return pointHistoryTable.selectAllByUserId(userId);
+  }
+}
